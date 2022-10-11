7ffb01d3c391cf7889c57029335a53d4ce0d5f81
Marta Iachini <iachinimarta@gmail.com>
2022-10-11 16:40:12 +0200

    Primo commit test

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::middleware('auth')
         Route::resource('posts', 'PostController');
     });
 
+
+    
 Route::get('{any}', function() {
     return view('guest.home');
 })->where('any', '.*');
